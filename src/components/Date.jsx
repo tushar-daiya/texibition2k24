cfba478813d84998beba16297a86a0571347c699
tushar daiya <tushardaiya@outlook.com>
2024-05-11 09:48:20 +0530

    updates

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Date = () => {
   return (
-    <div className="datebg opacity relative z-10 ml-[1.5rem] flex w-fit flex-col rounded-md bg-white p-2 pr-8 text-xl font-bold text-black md:ml-10  md:text-3xl">
-      <span >
-        2024 <span>May</span>
-      </span>
+    <div className="datebg  opacity relative z-10 ml-[1.5rem] flex w-fit flex-col rounded-md bg-white p-2 pr-8 text-xl font-bold text-black md:ml-10  md:text-3xl">
       <span className=" flex gap-2 text-[2rem] font-bold">
         28
         <b className="text-[16px]">TH</b>
         29
         <b className="text-[16px]">TH</b>
+      </span>
+      <span className="text-2xl tracking-widest">
+        2024 <span>May</span>
       </span>
     </div>
   );
